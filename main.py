69270b7ae1ea18dd058ee12de1a74fafb088976d
Elizeu Junior <131943477+elizeujrdev@users.noreply.github.com>
2024-03-18 00:22:31 -0300

    fixed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def inverte_string(string):
-    return string[::-1]
+def inverter_string(string):
+    inverted = ''
+    for char in string:
+        inverted = char + inverted
+    return inverted
 
 texto = input("Digite uma string: ")
-texto_invertido = inverte_string(texto)
+texto_invertido = inverter_string(texto)
 print("String invertida:", texto_invertido)
